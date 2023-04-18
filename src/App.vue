57089396701b7dd2f878254997292eb06bc58c19
Trung Hieu <hieu@ltv.dev>
2023-04-18 12:37:24 +0700

    Create Introduction UI

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <!-- Header -->
   <app-header></app-header>
+
+  <!-- Introduction -->
+  <introduction></introduction>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue'
+import Introduction from './components/Introduction.vue'
 
 export default {
   name: 'App',
   components: {
-    AppHeader
+    AppHeader,
+    Introduction
   }
 }
 </script>
