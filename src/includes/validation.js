5ca4670e74552d505ee3d1cef9c246a9f3fb82b8
Trung Hieu <hieu@ltv.dev>
2023-04-20 15:55:54 +0700

    Register-Form: Validation using Vee-Validate & Submit Function

---
+++ src/includes/validation.js
@@ -0,0 +1,65 @@
+import {
+  Field as VeeField,
+  Form as VeeForm,
+  defineRule,
+  ErrorMessage,
+  configure
+} from 'vee-validate'
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+  email,
+  min_value as minValue,
+  max_value as maxValue,
+  confirmed,
+  not_one_of as excluded
+} from '@vee-validate/rules'
+
+export default {
+  install(app) {
+    app.component('VeeField', VeeField)
+    app.component('VeeForm', VeeForm)
+    app.component('ErrorMessage', ErrorMessage)
+
+    defineRule('required', required)
+    defineRule('tos', required)
+    defineRule('min', min)
+    defineRule('max', max)
+    defineRule('alpha_spaces', alphaSpaces)
+    defineRule('email', email)
+    defineRule('min_value', minValue)
+    defineRule('max_value', maxValue)
+    defineRule('password_mismatch', confirmed)
+    defineRule('excluded', excluded)
+    defineRule('country_excluded', excluded)
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required.`,
+          min: `The field ${ctx.field} is too short.`,
+          max: `The field ${ctx.field} is too long.`,
+          alpha_spaces: `The field ${ctx.field} may only contain alphabetical.`,
+          email: `The field ${ctx.field} must be a valid email.`,
+          min_value: `The field ${ctx.field} is too low.`,
+          max_value: `The field ${ctx.field} is too high.`,
+          excluded: `You are not allow to use this value for this field ${ctx.field}.`,
+          country_excluded: `This country is not allowed. `,
+          password_mismatch: `The password don't match.`,
+          tos: `You must accept the Terms of Services.`
+        }
+
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is in valid`
+        return message
+      },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true
+    })
+  }
+}
