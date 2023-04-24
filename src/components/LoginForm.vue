83b6f79615e92be5345b547b6884ba01da07e669
Trung Hieu <hieu@ltv.dev>
2023-04-25 05:42:04 +0700

    Firebase: Log In

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -40,6 +40,9 @@
 </template>
 
 <script>
+import { mapActions } from 'pinia'
+import useUserStore from '@/stores/user'
+
 export default {
   name: 'LoginForm',
   data() {
@@ -55,15 +58,24 @@ export default {
     }
   },
   methods: {
-    login(values) {
+    ...mapActions(useUserStore, ['authenticate']),
+    async login(values) {
       this.login_show_alert = true
       this.login_in_submission = true
       this.login_alert_variant = 'bg-blue-500'
       this.login_alert_message = 'Please wait! We are logging you in.'
 
+      try {
+        await this.authenticate(values)
+      } catch (error) {
+        this.login_in_submission = false
+        this.login_alert_variant = 'bg-red-500'
+        this.login_alert_message = error.message
+        return
+      }
+
       this.login_alert_variant = 'bg-green-500'
       this.login_alert_message = 'Success! Your are logged.'
-      console.log(values)
     }
   }
 }
