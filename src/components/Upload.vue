fc02f81b25b41695268b82432aafa76d351ed19f
Trung Hieu <hieu@ltv.dev>
2023-04-28 16:51:09 +0700

    Vue Upload File

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -15,7 +15,7 @@
         @dragover.prevent.stop="is_dragover = true"
         @dragenter.prevent.stop="is_dragover = true"
         @dragleave.prevent.stop="is_dragover = false"
-        @drop.prevent.stop="upload"
+        @drop.prevent.stop="upload($event)"
       >
         <h5>Drop your files here</h5>
       </div>
@@ -54,8 +54,19 @@ export default {
     }
   },
   methods: {
-    upload() {
+    upload($event) {
       this.is_dragover = false
+
+      const files = [...$event.dataTransfer.files]
+
+      files.forEach((file) => {
+        if (file.type !== 'audio/mpeg') {
+          return
+        }
+      })
+
+      console.log($event.dataTransfer)
+      console.log(files)
     }
   }
 }
