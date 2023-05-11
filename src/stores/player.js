ae137c618d37d46e41416883a59a284eb65baa62
Trung Hieu <hieu@ltv.dev>
2023-05-11 08:46:37 +0700

    Playing Song: Format Song Time to Display on Progress Bar

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,10 +1,14 @@
 import { defineStore } from 'pinia'
 import { Howl } from 'howler'
 
+import helper from '@/includes/helper'
+
 export default defineStore('player', {
   state: () => ({
     current_song: {},
-    sound: {}
+    sound: {},
+    seek: '00:00',
+    duration: '00:00'
   }),
   actions: {
     async newSong(song) {
@@ -16,6 +20,23 @@ export default defineStore('player', {
       })
 
       this.sound.play()
+
+      // listen event playing
+      this.sound.on('play', () => {
+        // requestAnimationFrame() working like interval function
+        // execute the function passed into it
+        // get call before the next frame paint on the screen
+        requestAnimationFrame(this.progress)
+      })
+    },
+    progress() {
+      this.seek = helper.formatTime(this.sound.seek())
+      this.duration = helper.formatTime(this.sound.duration())
+
+      // recursive to continuously dispatch playing time
+      if (this.sound.playing()) {
+        requestAnimationFrame(this.progress)
+      }
     },
     async toggleAudio() {
       // check if Howl Object created or not
