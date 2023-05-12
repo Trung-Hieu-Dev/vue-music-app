4053c733e0be564da2df476fb1dd0c6c636792fe
Trung Hieu <hieu@ltv.dev>
2023-05-12 22:08:50 +0700

    Performance Optimizations: Reducing Bundle Size

--- src/components/base/Button.vue
+++ src/components/base/Button.vue
@@ -2,7 +2,7 @@
   <button
     class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
   >
-    <slot></slot>
+    <slot>Click me!</slot>
   </button>
 </template>
 
